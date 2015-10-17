8017acaf8c56192d425d6142bb5c4295abafe33e
mjvarghese <mjvarghese@optonline.net>
2015-10-17 10:02:18 +0000

    Completing Warm Up for week 8.

---
+++ task_02.py
@@ -0,0 +1,7 @@
+# /usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Task 2."""
+
+
+MY_INTEGER = raw_input('What is the answer to life, universe, and everything?')
+MY_INTEGER = int(MY_INTEGER)
--- task_04.py
+++ task_04.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""A single if statement."""
+"""Task 4."""
+
 
 MYINPUT = raw_input('Tell me a story! ')
 MAX_LENGTH = 80
 LONGSTR = 'short'
 
-# You code goes here
+LENGTH = len(MYINPUT)
+if LENGTH > MAX_LENGTH:
+    LONGSTR = 'long'
 
+else:
+    LONGSTR = 'short'
 OUTPUT = 'That certainly was a {} story!'.format(LONGSTR)
 print OUTPUT
