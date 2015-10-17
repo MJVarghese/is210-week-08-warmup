0da91fff1ae43a920f4788429dd37a21e0a4b753
mjvarghese <mjvarghese@optonline.net>
2015-10-17 10:07:24 +0000

    Second try.

--- task_05.py
+++ task_05.py
@@ -6,19 +6,19 @@
 BP = int(raw_input('What is your blood pressure? '))
 
 if BP <= 89:
-    BP_STATUS = 'Low!'
+    BP_STATUS = 'Low'
 
 elif BP >= 90 and BP <= 119:
-    BP_STATUS = 'Ideal!'
+    BP_STATUS = 'Ideal'
 
 elif BP >= 120 and BP <= 139:
-    BP_STATUS = 'Warning!'
+    BP_STATUS = 'Warning'
 
 elif BP >= 140 and BP <= 159:
-    BP_STATUS = 'High!'
+    BP_STATUS = 'High'
 
 else:
-    BP_STATUS = 'Emergency!'
+    BP_STATUS = 'Emergency'
 
 OUTPUT = 'Your status is currently: {}'.format(BP_STATUS)
 print OUTPUT
